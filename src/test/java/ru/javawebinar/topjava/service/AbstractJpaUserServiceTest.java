bce952e2e036e77be31423a8f1bcc0e5063f32bc
AleksCrow <Qwerton00@gmail.com>
2019-11-22 12:32:08 +0200

    HW06

--- src/test/java/ru/javawebinar/topjava/service/AbstractJpaUserServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/AbstractJpaUserServiceTest.java
@@ -25,6 +25,7 @@ abstract public class AbstractJpaUserServiceTest extends AbstractUserServiceTest
 
     @Test
     public void createWithException() throws Exception {
+
         validateRootCause(() -> service.create(new User(null, "  ", "mail@yandex.ru", "password", Role.ROLE_USER)), ConstraintViolationException.class);
         validateRootCause(() -> service.create(new User(null, "User", "  ", "password", Role.ROLE_USER)), ConstraintViolationException.class);
         validateRootCause(() -> service.create(new User(null, "User", "mail@yandex.ru", "  ", Role.ROLE_USER)), ConstraintViolationException.class);
